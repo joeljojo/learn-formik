adaf00e186cfc9e3bf3349516efcbffc3a7a397e
joeljojo <joelpamphyl@gmail.com>
2023-10-24 00:19:30 +0300

    Add validation using Yup

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import {
 } from "@mui/material";
 import "./App.css";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function App() {
   const formik = useFormik({
@@ -18,6 +19,15 @@ function App() {
       email: "",
       password: "",
     },
+    validationSchema: Yup.object({
+      firstName: Yup.string().required("First name is required"),
+      lastName: Yup.string().required("Last name is required"),
+      email: Yup.string().email().required("Email is required"),
+      password: Yup.string()
+        .required("Password is required")
+        .min(4, "Password must have at least 4 characters")
+        .max(8, "Password must not have more than 8 characters"),
+    }),
     onSubmit: (values) => {
       console.log(values);
     },
@@ -37,6 +47,9 @@ function App() {
               onChange={formik.handleChange}
               value={formik.values.firstName}
             />
+            {formik.touched.firstName && formik.errors.firstName && (
+              <Grid sx={{ color: "red" }}>{formik.errors.firstName}</Grid>
+            )}
           </FormControl>
           <FormControl>
             <FormLabel htmlFor="lastName">Last Name</FormLabel>
@@ -48,6 +61,9 @@ function App() {
               onChange={formik.handleChange}
               value={formik.values.lastName}
             />
+            {formik.touched.lastName && formik.errors.lastName && (
+              <Grid sx={{ color: "red" }}>{formik.errors.lastName}</Grid>
+            )}
           </FormControl>
           <FormControl>
             <FormLabel htmlFor="email">Email</FormLabel>
@@ -59,6 +75,9 @@ function App() {
               onChange={formik.handleChange}
               value={formik.values.email}
             />
+            {formik.touched.email && formik.errors.email && (
+              <Grid sx={{ color: "red" }}>{formik.errors.email}</Grid>
+            )}
           </FormControl>
           <FormControl>
             <FormLabel htmlFor="password">Password</FormLabel>
@@ -70,6 +89,9 @@ function App() {
               onChange={formik.handleChange}
               value={formik.values.password}
             />
+            {formik.touched.password && formik.errors.password && (
+              <Grid sx={{ color: "red" }}>{formik.errors.password}</Grid>
+            )}
           </FormControl>
           <Button type="submit">Register</Button>
         </Stack>
